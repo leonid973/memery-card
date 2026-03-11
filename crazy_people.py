7633c18ae1dddc20f72109dd1e0266784ff17b52
leonid973 <leonidmobile480@gmail.com>
2026-03-11 20:51:18 +0300

    Create crazy_people.py

---
+++ crazy_people.py
@@ -0,0 +1,46 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QLabel,QHBoxLayout,QRadioButton,QGroupBox,QPushButton
+app = QApplication([])
+question = QLabel('Какой национальности не существует?')
+
+but = QPushButton('Ответить')
+
+group = QGroupBox('Варианты ответов')
+
+window = QWidget()
+
+firstlayout = QVBoxLayout()
+secondlayout = QHBoxLayout()
+thirdlayout = QHBoxLayout()
+
+rbtn1 = QRadioButton('Энцы')
+rbtn2 = QRadioButton('Смурфы')
+rbtn3 = QRadioButton('Чулымцы')
+rbtn4 = QRadioButton('Алеуты')
+
+layoutans1 = QHBoxLayout()
+layoutans2 = QVBoxLayout()
+layoutans3 = QVBoxLayout()
+
+layoutans2.addWidget(rbtn1)
+layoutans2.addWidget(rbtn2)
+
+layoutans3.addWidget(rbtn3)
+layoutans3.addWidget(rbtn4)
+
+layoutans1.addLayout(layoutans2)
+layoutans1.addLayout(layoutans3,stretch = 2)
+
+secondlayout.addWidget(question)
+
+group.setLayout(layoutans1)
+thirdlayout.addWidget(but)
+
+firstlayout.addLayout(secondlayout)
+firstlayout.addWidget(group)
+firstlayout.addLayout(thirdlayout)
+
+window.setLayout(firstlayout)
+
+window.show()
+app.exec()
